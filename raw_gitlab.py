4685280969e565d94c9a18185ff13aec2caeea18
trocupocu <133394729+trocupocu@users.noreply.github.com>
2025-05-07 19:38:10 +0300

    Update raw_gitlab.py

--- raw_gitlab.py
+++ raw_gitlab.py
@@ -2,8 +2,8 @@ from seleniumbase import SB
 
 with SB(uc=True, test=True, locale_code="en") as sb:
     url = "https://gitlab.com/users/sign_in"
-    sb.cdp.save_screenshot("ssasa.png", folder='./latest_logs')
     sb.activate_cdp_mode(url)
+    sb.cdp.save_screenshot("ssasa.png", folder='./latest_logs')
     sb.uc_gui_click_captcha()
     sb.cdp.save_screenshot("ssasa2.png", folder='./latest_logs')
     sb.assert_text("Username", '[for="user_login"]', timeout=3)
