162f90f1b7bdb7317d309b22c7b4531f9587cb8f
trocupocu <133394729+trocupocu@users.noreply.github.com>
2025-05-07 19:10:35 +0300

    Update my_socialblade.py

--- my_socialblade.py
+++ my_socialblade.py
@@ -60,13 +60,9 @@ with SB(uc=True, test=True, locale_code="en", headless=False) as sb:
     self.activate_cdp_mode(
         url,
         platform='Win32',
-        user_agent=(
-            url,
-            platform='Win32',
-            user_agent=("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                        "AppleWebKit/537.36 (KHTML, like Gecko) "
-                        "Chrome/135.0.0.0 Safari/537.36"
-            )
+        user_agent=("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                    "Chrome/135.0.0.0 Safari/537.36"
         )
     )
     rnd = random.randint(4, 7)
