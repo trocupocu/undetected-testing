aad7ac1f74c4a981333a65bd26662411e228a6c9
trocupocu <133394729+trocupocu@users.noreply.github.com>
2025-05-07 19:47:34 +0300

    Update raw_gitlab.py

--- raw_gitlab.py
+++ raw_gitlab.py
@@ -2,8 +2,10 @@ from seleniumbase import SB
 
 with SB(uc=True, test=True, locale_code="en") as sb:
     url = "https://gitlab.com/users/sign_in"
-    sb.activate_cdp_mode(url)
+    sb.uc_open_with_reconnect(url, 5)
     sb.uc_gui_click_captcha()
+    sb.sleep(2)
+    sb.uc_gui_handle_captcha()
     sb.assert_text("Username", '[for="user_login"]', timeout=3)
     sb.assert_element('label[for="user_login"]')
     sb.highlight('button:contains("Sign in")')
