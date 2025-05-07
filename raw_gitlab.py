83853dbfeae0f2097c0c2643e1b9ae797bcb171d
trocupocu <133394729+trocupocu@users.noreply.github.com>
2025-05-07 19:33:47 +0300

    Update raw_gitlab.py

--- raw_gitlab.py
+++ raw_gitlab.py
@@ -1,9 +1,11 @@
 from seleniumbase import SB
 
 with SB(uc=True, test=True, locale_code="en") as sb:
-    url = "https://www.youtube.com/@PewDiePie/videos"
+    url = "https://gitlab.com/users/sign_in"
+    sb.cdp.save_screenshot("ssasa.png", folder='./latest_logs')
     sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
+    sb.cdp.save_screenshot("ssasa2.png", folder='./latest_logs')
     sb.assert_text("Username", '[for="user_login"]', timeout=3)
     sb.assert_element('label[for="user_login"]')
     sb.highlight('button:contains("Sign in")')
